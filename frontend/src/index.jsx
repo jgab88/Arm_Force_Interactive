2166841c92599b0a5237f5155906529486ad38f9
jG <jerry.g@sipptech.com>
2025-03-04 20:58:31 -0600

    tailwind3 proper install.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './output.css';  // Import the CSS directly
+import './index.css';  // Import the CSS directly
 
 const root = createRoot(document.getElementById('root'));
 root.render(
